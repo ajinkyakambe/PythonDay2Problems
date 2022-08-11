8dcf134e5a9e09c370d54d1b6727fa060f212870
ajinkyakambe <ajinkyakambe@gmail.com>
2022-08-12 01:15:28 +0530

    Added: Ability to create a Contacts in Address Book

--- address_book_main.py
+++ address_book_main.py
@@ -1,3 +1,44 @@
 # Start of Address Book Program
 
-print("Welcome to Address Book Program")
+'''
+Ability to create a Contacts in Address
+Book with first and last names, address,
+city, state, zip, phone number and
+email...
+'''
+
+
+class Contacts():
+
+    def __init__(self, first_name, last_name, address, city, state, zip, phone_number, email):
+        """
+            Description: Constructor of Contacts Class
+            Parametres: Takes class fields
+            Retuns: None, Just initialize the values of object
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.phone_number = phone_number
+        self.email = email
+
+    #https://www.educative.io/answers/what-is-the-str-method-in-python
+    def __str__(self):
+        """
+            Description: To return textual content of the Contacts class Object
+            Parameters: Takes Instance (Object) of class as arguments
+            Returns: Returns String Representation of object
+        """
+        return f"Full Name is {self.first_name} {self.last_name}\nFull address is {self.address},{self.city},{self.state},{self.zip}\nPhone Number & email is {self.phone_number} and {self.email} respectively"
+
+
+if __name__ == "__main__":
+    sampleContact = Contacts("Ajinkya", "Kambe", "koaknwadi,sbi", "Murtizapur",
+                     "Maharashtra", 444107, 234234234, "ajinkyakambe@gmail.com")
+    try:
+        print(str(sampleContact))
+    except Exception as ex:
+        print(ex)
