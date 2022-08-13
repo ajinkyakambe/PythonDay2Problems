52984d593cfef2d25d31b116ca45437f4b008510
ajinkyakambe <ajinkyakambe@gmail.com>
2022-08-13 07:23:22 +0530

    Added: Ability to Add Multiple contacts from console.

--- address_book_main.py
+++ address_book_main.py
@@ -1,38 +1,8 @@
 # Start of Address Book Program
 
-'''
-Ability to create a Contacts in Address
-Book with first and last names, address,
-city, state, zip, phone number and
-email...
-'''
+# Importing Contacts Class
+from contacts import Contacts
 
-
-class Contacts():
-
-    def __init__(self, first_name, last_name, address, city, state, zip, phone_number, email):
-        """
-            Description: Constructor of Contacts Class
-            Parametres: Takes class fields
-            Retuns: None, Just initialize the values of object
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zip = zip
-        self.phone_number = phone_number
-        self.email = email
-
-    # https://www.educative.io/answers/what-is-the-str-method-in-python
-    def __str__(self):
-        """
-            Description: To return textual content of the Contacts class Object
-            Parameters: Takes Instance (Object) of class as arguments
-            Returns: Returns String Representation of object
-        """
-        return f"Full Name is {self.first_name} {self.last_name}\nFull address is {self.address},{self.city},{self.state},{self.zip}\nPhone Number & email is {self.phone_number} and {self.email} respectively"
 
 
 def add_contact():
@@ -42,16 +12,18 @@ def add_contact():
             Returns: Returns contact class object
     """
     print("Adding the contact")
-    first_name = input("Enter the first name: ")
-    last_name = input("Enter the last name: ")
-    address = input("Enter the address: ")
-    city = input("Enter city name: ")
-    state = input("Enter state name: ")
-    zip = input("Enter zip code: ")
-    phone_number = input("Enter phone_number: ")
-    email = input("Enter email address: ")
-    contact_obj = Contacts(first_name, last_name, address,
-                           city, state, zip, phone_number, email)
+    while (True):
+        first_name = input("Enter the first name: ")
+        last_name = input("Enter the last name: ")
+        address = input("Enter the address: ")
+        city = input("Enter city name: ")
+        state = input("Enter state name: ")
+        zip = input("Enter zip code: ")
+        phone_number = input("Enter phone_number: ")
+        email = input("Enter email address: ")
+        contact_obj = Contacts(first_name, last_name, address,
+                               city, state, zip, phone_number, email)
+        break
     return contact_obj
 
 
