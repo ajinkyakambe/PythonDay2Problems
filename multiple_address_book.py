2ec2cb31c7d3d2e18a27954fa02fa78abaf5644d
ajinkyakambe <ajinkyakambe@gmail.com>
2022-08-15 02:08:36 +0530

    Added: Added City and State Dictionaries

--- multiple_address_book.py
+++ multiple_address_book.py
@@ -27,6 +27,7 @@ def adding_contactsdata_in_dictionary(addressbook_dict):
         print(ex)
         adding_contactsdata_in_dictionary(addressbook_dict)
 
+
 def search_person_in_a_city_state(searching_dict):
     entered_city_or_state = input(
         "Please enter a city or state for searching a person: ")
@@ -38,6 +39,28 @@ def search_person_in_a_city_state(searching_dict):
                 print(str(item))
 
 
+def dictionary_of_city_and_person(searching_dict):
+    try:
+        city_person_dict = {}
+        for contacts_list in searching_dict.values():
+            list(map(lambda x: city_person_dict.setdefault(
+                x.city, []).append(x), contacts_list))
+        return city_person_dict
+    except Exception as ex:
+        print(ex)
+
+
+def dictionary_of_state_and_person(searching_dict):
+    try:
+        state_person_dict = {}
+        for contacts_list in searching_dict.values():
+            list(map(lambda x: state_person_dict.setdefault(
+                x.state, []).append(x), contacts_list))
+        return state_person_dict
+    except Exception as ex:
+        print(ex)
+
+
 if __name__ == "__main__":
     address_book_dict = {}
     adding_contactsdata_in_dictionary(address_book_dict)
@@ -45,3 +68,15 @@ if __name__ == "__main__":
         print("Address Book Name is: ", key)
         for item in value:
             print(str(item))
+
+    # Will be displaying the dic of person by city and state
+    cityperson_dict = dictionary_of_city_and_person(address_book_dict)
+    for key, value in cityperson_dict.items():
+        print("Persons residing in ", key, " city are:")
+        for each_contact in value:
+            print(str(each_contact))
+    stateperson_dict = dictionary_of_state_and_person(address_book_dict)
+    for key, value in stateperson_dict.items():
+        print("Persons residing in ", key, " state are:")
+        for each_contact in value:
+            print(str(each_contact))
