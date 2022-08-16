8d7a7d540a3990cfbc4e7a7158f511cd4e7bc74f
ajinkyakambe <ajinkyakambe@gmail.com>
2022-08-16 23:49:04 +0530

    Added: Added sorted by city,state,zip method and refactored other code

--- multiple_address_book.py
+++ multiple_address_book.py
@@ -39,7 +39,7 @@ def search_person_in_a_city_state(searching_dict):
         "Please enter a city or state for searching a person: ")
     for key, value in searching_dict.items():
         for item in value:
-            if (item.city == entered_city_or_state or item.state == entered_city_or_state):
+            if item.city == entered_city_or_state or item.state == entered_city_or_state:
                 print("The person, you searched for, belongs to ",
                       entered_city_or_state, " & is from this ", key, " address book: ")
                 print(str(item))
@@ -71,9 +71,7 @@ def sorting_entries_by_person_name(address_book_dict):
     # https://www.w3schools.com/python/python_lambda.asp
     try:
         contacts_list = list(address_book_dict.values())
-        sorted_list = reduce(lambda x, y: x + y, contacts_list)
-
-        # sorted_list = reduce(operator.add, contacts_list)
+        sorted_list = reduce(operator.add, contacts_list)
         sorted_list.sort(key=lambda x: x.first_name)
         return sorted_list
     except Exception as ex:
@@ -87,6 +85,36 @@ def printing_details_on_console(list_to_be_printed):
     print(space)
 
 
+def sorting_entries_by_city(address_book_dict):
+    try:
+        contacts_list = list(address_book_dict.values())
+        sorted_list = reduce(operator.add, contacts_list)
+        sorted_list.sort(key=lambda x: x.city)
+        return sorted_list
+    except Exception as ex:
+        print(ex)
+
+
+def sorting_entries_by_state(address_book_dict):
+    try:
+        contacts_list = list(address_book_dict.values())
+        sorted_list = reduce(operator.add, contacts_list)
+        sorted_list.sort(key=lambda x: x.state)
+        return sorted_list
+    except Exception as ex:
+        print(ex)
+
+
+def sorting_entries_by_zip(address_book_dict):
+    try:
+        contacts_list = list(address_book_dict.values())
+        sorted_list = reduce(operator.add, contacts_list)
+        sorted_list.sort(key=lambda x: x.zip)
+        return sorted_list
+    except Exception as ex:
+        print(ex)
+
+
 # Global space variable
 space = "|---------------------------|"
 
@@ -95,11 +123,14 @@ if __name__ == "__main__":
     while True:
         user_input = int(input(
             "\"1\" for adding Contact Details...\n\
-            \"2\" for searching the person in city or state...\n\
-            \"3\" for viewing person from a city...\n\
-            \"4\" for viewing person from a state...\n\
-            \"5\" for sorting person by first name...\n\
-            \"ANY OTHER KEY\" for exiting..."
+                        \"2\" for searching the person in city or state...\n\
+                        \"3\" for viewing person from a city...\n\
+                        \"4\" for viewing person from a state...\n\
+                        \"5\" for sorting person by first name...\n\
+                        \"6\" for sorting person by city name...\n\
+                        \"7\" for sorting person by state name...\n\
+                        \"8\" for sorting person by zip...\n\
+                        \"ANY OTHER KEY\" for exiting..."
         ))
 
         if user_input == 1:
@@ -137,12 +168,18 @@ if __name__ == "__main__":
             printing_details_on_console(sorted_name_list)
             continue
 
-        if user_input is not 1 or 2 or 3 or 4 or 5:
+        if user_input == 6:
+            sorted_by_city_list = sorting_entries_by_city(address_book_dict)
+            printing_details_on_console(sorted_by_city_list)
+            continue
+        if user_input == 7:
+            sorted_by_state_list = sorting_entries_by_state(address_book_dict)
+            printing_details_on_console(sorted_by_state_list)
+            continue
+        if user_input == 8:
+            sorted_by_zip_list = sorting_entries_by_zip(address_book_dict)
+            printing_details_on_console(sorted_by_zip_list)
+            continue
+        if user_input is not 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8:
             break
-
-
-
-
-
-
 
